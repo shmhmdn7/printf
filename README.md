5037870faa9d223497f265664125264359e78de3
shmhmdn7 <141265831+shmhmdn7@users.noreply.github.com>
2023-10-16 23:19:36 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-read
+Group project to create a printf function
+It is a very important and versatile function in the C programming language and being about to create our custom version of it will go a long way to enhance our understanding of the language.
+
+printf project includes these basic concepts:
+The format string.
+Argument handlling.
+Processing format specifier.
+Converting and Formating.
+Output generation.
+Modifiers and Special cases.
+Memory mangment.
+
+Tasks:
+0. To write a function that produces output according to a format for specifier, c - s - %.
+1. To handle the following conversion specifiers, d - i.
---
+++ main.h
@@ -0,0 +1,58 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags_t - struct containing flags
+ * @plus: '+' character
+ * @space: ' ' character
+ * @hash: '#' character
+ * @zero: '0' character
+ * @minus: '-' character
+ */
+typedef struct flags_t
+{
+	int plus;
+	int space;
+	int hash;
+	int zero;
+	int minus;
+} flags_t;
+
+/**
+ * struct ph - struct
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct ph
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+
+int _printf(const char *format, ...);
+int (*_pr_spf(char sp))(va_list, flags_t *);
+int _write(char c);
+int _write_str(char *s);
+int pr_string(va_list a, flags_t *f);
+int pr_char(va_list a, flags_t *f);
+int pr_percent(va_list a, flags_t *f);
+int p_flag(char sp, flags_t *f);
+int pr_int(va_list a, flags_t *f);
+void pr_num(int n);
+int _length(int i);
+int pr_unsigned(va_list a, flags_t *f);
+int pr_binary(va_list a, flags_t *f);
+int pr_octal(va_list a, flags_t *f);
+int pr_hex(va_list a, flags_t *f);
+int pr_hex_big(va_list a, flags_t *f);
+int pr_bigS(va_list a, flags_t *f);
+int pr_address(va_list a, flags_t *f);
+int pr_rev(va_list a, flags_t *f);
+int pr_rot13(va_list a, flags_t *f);
+char *convert(unsigned long int n, int b, int lc);
+
+#endif
